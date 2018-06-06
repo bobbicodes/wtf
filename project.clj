c52fb6b3f422114a53297073553f1236ae163f83
Bobby Towers <porkostomus@mail.com>
2018-06-05 20:08:03 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject shouter "0.0.2"
-  :description "Shouter app"
-  :url "https://github.com/technomancy/shouter"
+(defproject wtf "0.0.2"
+  :description "WTF app"
+  :url "https://github.com/porkostomus/wtf"
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -12,8 +12,8 @@
 		 [org.clojure/data.json "0.2.6"]
                  [ring/ring-defaults "0.1.2"]
                  [hiccup "1.0.5"]]
-  :main ^:skip-aot shouter.core
-  :uberjar-name "shouter-standalone.jar"
+  :main ^:skip-aot wtf.core
+  :uberjar-name "wtf-standalone.jar"
   :plugins [[lein-ring "0.8.13"]]
 
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
